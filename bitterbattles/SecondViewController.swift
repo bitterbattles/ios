a6fd8c970c0a78ac31a57ad6a34f7421d551aad5
Adam Lupinacci <adam@Adams-MBP.local.tld>
2019-02-21 07:05:38 -0800

    Initial add of project files

---
+++ bitterbattles/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  bitterbattles
+//
+//  Created by Adam Lupinacci on 2/21/19.
+//  Copyright © 2019 Little Wolf Software. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
